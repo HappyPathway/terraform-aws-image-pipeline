14fe6e5ecb9e642b525e718ac49f94d7323bae6d
Dave Arnold <dave@roknsound.com>
2024-05-14 14:17:43 -0700

    one

--- sec-group.tf
+++ sec-group.tf
@@ -14,20 +14,20 @@ resource "aws_security_group_rule" "sg_rule" {
   from_port         = 0
   to_port           = 65535
   protocol          = "tcp"
-  security_group_id = aws_security_group.packer.id
-  source_security_group_id = aws_security_group.packer.id
+  security_group_id = one(aws_security_group.packer).id
+  source_security_group_id = one(aws_security_group.packer).id
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   count       = var.vpc_config == null ? 0 : 1
-  security_group_id = aws_security_group.packer.id
+  security_group_id = one(aws_security_group.packer).id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   count       = var.vpc_config == null ? 0 : 1
-  security_group_id = aws_security_group.packer.id
+  security_group_id = one(aws_security_group.packer).id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
