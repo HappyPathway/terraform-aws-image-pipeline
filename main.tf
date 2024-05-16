ecedf5efe63b8e1a7de2c96393336f05a2662244
Dave Arnold <dave@roknsound.com>
2024-05-16 10:13:19 -0700

    adding variables

--- main.tf
+++ main.tf
@@ -82,18 +82,18 @@ module "codebuild_terraform" {
   vpc_config                          = local.vpc_config
   environment_variables = concat(
     var.build_environment_variables,
-    # [
-    #   {
-    #     name  = "AWS_SECRET_ACCESS_KEY",
-    #     value = aws_secretsmanager_secret.credentials.arn,
-    #     type  = "SECRETS_MANAGER"
-    #   },
-    #   {
-    #     name  = "AWS_ACCESS_KEY_ID",
-    #     value = aws_iam_access_key.build_user.id,
-    #     type  = "PLAINTEXT"
-    #   }
-    # ]
+    [
+      {
+        name  = "PKR_VAR_security_group_id",
+        value = one(var.vpc_config.security_group_ids),
+        type = "PLAINTEXT"
+      },
+      {
+        name  = "PKR_VAR_subnet_id",
+        value = one(var.vpc_config.subnets),
+        type = "PLAINTEXT"
+      }
+    ]
     []
   )
   kms_key_arn = module.codepipeline_kms.arn
