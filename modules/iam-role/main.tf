eca77c7c5a9a72aed9fb2aaba1fdc0df8d26437c
Dave Arnold <dave@roknsound.com>
2024-06-11 14:22:36 -0700

    chore: Update IAM role permissions for SSM access and fix SSM resource ARN in main.tf

--- modules/iam-role/main.tf
+++ modules/iam-role/main.tf
@@ -73,7 +73,9 @@ data "aws_iam_policy_document" "codepipeline_policy" {
     actions = [
       "ssm:*"
     ]
-    resources = ["arn::${data.aws_partition.current.partition}:ssm:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:parameter/image-pipeline/${var.environment}/${var.project_name}/*"]
+    resources = [
+      "arn:${data.aws_partition.current.partition}:ssm:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:parameter/image-pipeline/${var.environment}/${var.project_name}/*"
+    ]
   }
 
   statement {
