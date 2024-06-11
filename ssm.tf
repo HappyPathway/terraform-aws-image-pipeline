5fbab439833858acb631fbf2253e33861339e1b0
Dave Arnold <dave@roknsound.com>
2024-06-11 12:26:11 -0700

    chore: Update SSM parameter names for security group IDs, subnets, and VPC ID

--- ssm.tf
+++ ssm.tf
@@ -1,17 +1,17 @@
 resource "aws_ssm_parameter" "security_group_id" {
-  name  = "image-pipeline/${var.environment}/${var.project_name}/security_group_ids"
+  name  = "/image-pipeline/${var.environment}/${var.project_name}/security_group_ids"
   type  = "StringList"
   value = join(",", var.vpc_config.security_group_ids)
 }
 
 resource "aws_ssm_parameter" "subnets" {
-  name  = "image-pipeline/${var.environment}/${var.project_name}/subnets"
+  name  = "/image-pipeline/${var.environment}/${var.project_name}/subnets"
   type  = "StringList"
   value = join(",", var.vpc_config.subnets)
 }
 
 resource "aws_ssm_parameter" "vpc_id" {
-  name  = "image-pipeline/${var.environment}/${var.project_name}/vpc_id"
+  name  = "/image-pipeline/${var.environment}/${var.project_name}/vpc_id"
   type  = "String"
   value = var.vpc_config.vpc_id
 }
