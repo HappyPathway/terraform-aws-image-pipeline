2afcf3779bc87c4e647b3b6742c93118b5851663
Dave Arnold <dave@roknsound.com>
2024-06-13 13:27:47 -0700

    chore: Update SSM configuration to use local variables instead of input variables

--- ssm.tf
+++ ssm.tf
@@ -1,25 +1,25 @@
 resource "aws_ssm_parameter" "security_group_id" {
   name  = "/image-pipeline/${var.environment}/${var.project_name}/security_group_ids"
   type  = "StringList"
-  value = join(",", var.vpc_config.security_group_ids)
+  value = join(",", local.vpc_config.security_group_ids)
 }
 
 resource "aws_ssm_parameter" "region" {
   name  = "/image-pipeline/${var.environment}/${var.project_name}/region"
   type  = "String"
-  value = var.vpc_config.region
+  value = local.vpc_config.region
 }
 
 resource "aws_ssm_parameter" "subnets" {
   name  = "/image-pipeline/${var.environment}/${var.project_name}/subnets"
   type  = "StringList"
-  value = join(",", var.vpc_config.subnets)
+  value = join(",", local.vpc_config.subnets)
 }
 
 resource "aws_ssm_parameter" "vpc_id" {
   name  = "/image-pipeline/${var.environment}/${var.project_name}/vpc_id"
   type  = "String"
-  value = var.vpc_config.vpc_id
+  value = local.vpc_config.vpc_id
 }
 
 resource "aws_ssm_parameter" "ssh_user" {
