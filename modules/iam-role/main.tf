0e9f8eaf86a4db859b9ae39e57ac499427f7e876
Dave Arnold <dave@roknsound.com>
2024-08-23 15:54:48 -0700

    Refactor IAM role permissions to use correct casing for ECR actions

--- modules/iam-role/main.tf
+++ modules/iam-role/main.tf
@@ -126,7 +126,7 @@ data "aws_iam_policy_document" "codepipeline_policy" {
   statement {
     effect = "Allow"
     actions = [
-      "ecr:getAuthorizationToken"
+      "ecr:GetAuthorizationToken"
     ]
     resources = [
       "*"
